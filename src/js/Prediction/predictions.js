e6c8b25eb5cb7e8d330138dec11ebc5f7f041bde
Anton-Mosquito <anton0886@outlook.com>
2020-09-21 10:51:19 +0300

    predictions was modified

--- src/js/Prediction/predictions.js
+++ src/js/Prediction/predictions.js
@@ -3,13 +3,18 @@ const btn = document.querySelector(".generateNumberOfPridict");
 const btn1 = document.querySelector(".showPridictButton");
 
 const predictions = [
-  "у вас сегодня будет замечательный день",
-  "сегодня лучше не выходите из дома",
-  "сегодня вас ждет удача и успех во всех начинаниях",
-  "сегодня вас ждет успех при изучении JavaScript",
-  "сегодня лучше полежите весь день на диване",
-  "сегодня вы рискуете что-нибудь забыть при выходе из дома",
-  "устройте сегодня себе выходной - пусть весь мир подождет",
+  [
+    "у вас сегодня будет замечательный день",
+    "сегодня вас ждет удача и успех во всех начинаниях",
+    "сегодня вас ждет успех при изучении JavaScript",
+    "устройте сегодня себе выходной - пусть весь мир подождет",
+  ],
+  [
+    "сегодня лучше не выходите из дома",
+    "сегодня лучше полежите весь день на диване",
+    "сегодня вы рискуете что-нибудь забыть при выходе из дома",
+    "удача сегодня отдыхает , релакс мен!",
+  ],
 ];
 
 class Predictions {
@@ -26,20 +31,26 @@ class Predictions {
 
   getDigits() {
     let random;
+    let randomMood;
     let timerID;
     this.input.value = "?";
     this.generateButton.addEventListener("click", () => {
+      randomMood = this.generateRandom(1, this.predictions.length);
       timerID = setInterval(() => {
-        this.input.value = random = this.generateRandom(
-          1,
-          this.predictions.length
-        );
+        this.input.value = random = this.generateRandom(1, 4);
       }, 100);
       this.generateButton.style.display = "none";
     });
     this.showResult.addEventListener("click", () => {
       clearInterval(timerID);
-      this.input.value = this.predictions[random - 1];
+      if (randomMood == 1) {
+        this.input.classList.add("green", "accent-3");
+        this.input.setAttribute("disabled", "disabled");
+      } else {
+        this.input.classList.add("red", "accent-3");
+        this.input.setAttribute("disabled", "disabled");
+      }
+      this.input.value = this.predictions[randomMood - 1][random - 1];
       this.showResult.style.display = "none";
     });
   }
